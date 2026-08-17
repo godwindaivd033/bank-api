1d55896babca2051855d194b627ac35e939f4d6d
Godwin David <godwindaivd@gmail.com>
2026-08-17 12:04:03 -0700

    Adjusted the resdis client test

--- test/test_redis.py
+++ test/test_redis.py
@@ -1,13 +1,20 @@
-import redis
+from fastapi import APIRouter
+from app.redis_client import redis_client
+
+
+
+router = APIRouter()
 
 
 
 
-client= redis.Redis(host= "localhost",
-                    port= 6379,
-                    decode_responses= True)
 
-client.set("message", "Hello Redis")
 
-value= client.get("message")
-print(value)
+@router.get("/test-redis")
+def test_resdis():
+    redis_client.set("message", "Hello from fastapi")
+
+    redis_client.set("message", "Hello Redis")
+
+    value= redis_client.get("message")
+    return {"message": value}
